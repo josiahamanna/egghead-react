71e7601303fc75ccec4396ff5e8187a0f153c756
JNA <josiah.amanna@niveussolutions.com>
2020-04-12 02:23:48 +0530

    Build a JSX Live Compiler as a React Component

---
+++ 16-Build-a-JSX-Live-Compiler-as-a-React-Component.js
@@ -0,0 +1,47 @@
+import React from "react";
+import "./styles.css";
+
+// Babel transpiling
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      err: "",
+      output: "",
+      imput: "/** put here jsx here ***/"
+    };
+  }
+  update(e) {
+    let code = e.target.value;
+    try {
+      this.setState({
+        // this is coming from the Babel object which you have CDN'ed
+        output: window.Babel.transform(code, { presets: ["es2015", "react"] })
+          .code,
+        err: ""
+      });
+    } catch (err) {
+      this.setState({ err: err.message });
+    }
+  }
+  render() {
+    return (
+      <div>
+        {/** Show the errors in the header */}
+        <header>{this.state.err}</header>
+        <div classname="container">
+          {/* place for input. write jsx here */}
+          <textarea
+            onChange={this.update.bind(this)}
+            defaultValue={this.state.input}
+          />
+          {/* Output is shown here */}
+          <pre>{this.state.output}</pre>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
+
